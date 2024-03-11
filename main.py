c723e553d206077a763a45a2a617fb3742b8759e
KAB47 <162886757+KAB47@users.noreply.github.com>
2024-03-11 12:05:53 +0000

    Add

---
+++ main.py
@@ -0,0 +1,9 @@
+# Define the main function (optional)
+def git_task_project():
+    greeting_message = input("Write here what you want to print on screen: ")   # Main function logic goes here
+    print(greeting_message)
+
+# Check if the script is executed as the main module
+if __name__ == "__main__":
+    # Call the main function (if needed)
+    git_task_project()
